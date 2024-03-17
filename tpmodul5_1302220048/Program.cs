b5c85f11a0b853d599b39a074889537a2bfbeee8
Ramadhana <ramadhanaa44@gmail.com>
2024-03-18 00:47:15 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302220048/Program.cs
+++ tpmodul5_1302220048/Program.cs
@@ -6,14 +6,28 @@
     }
 }
 
+public class DataGeneric<T>
+{
+    private T Data;
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+    }
+}
 
 class Program
 {
     private static void Main(string[] args)
     {
         HaloGeneric halo = new HaloGeneric();
-        halo.SapaUser<String>("Reihan Ramdhana");
+        halo.SapaUser<String>("Reihan Ramadhana");
 
-       
+        DataGeneric<string> dataGeneric = new DataGeneric<string>("1302220048");
+        dataGeneric.PrintData();
+
     }
 }
